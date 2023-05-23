5c3243a21056b1dc5a363bb92141c7f5b80c3035
TheBest1022 <fernando07101@outlook.com>
2023-05-23 11:24:00 -0500

    Actualizar

--- src/helpers/configuracion.js
+++ src/helpers/configuracion.js
@@ -2,4 +2,4 @@ const urlWeb = "https://app-backend-server.herokuapp.com/"
 const urlLocalCG = "http://192.168.0.104:4000/"
 const urlLocalTru = "http://192.168.0.3:4000/"
 const urilocal2 = "http://192.168.18.96:4000/"
-export const conexionURL = urlLocalTru
+export const conexionURL = urlWeb
