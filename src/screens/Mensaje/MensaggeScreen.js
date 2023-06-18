0807a2c4a73e83817437e2044d7ac77ed89d2923
TheBest1022 <fernando07101@outlook.com>
2023-06-18 14:46:02 -0500

    Actualizar

--- src/screens/Mensaje/MensaggeScreen.js
+++ src/screens/Mensaje/MensaggeScreen.js
@@ -16,17 +16,21 @@ import {
 import Name from "../../components/General";
 
 const MensaggeScreen = ({ route }) => {
-  const enviar = {uri:"https://www.dropbox.com/s/zcr44jc6erdb6rn/send.png?dl=1"}
+  const enviar = {
+    uri: "https://www.dropbox.com/s/zcr44jc6erdb6rn/send.png?dl=1",
+  };
   const navegation = useNavigation();
   const { auth, newMenssage, obtenerMensaje, MensajeActualizar } = useGlobal();
   const [loading, setLoading] = useState(false);
   const [dataMensaje, setDataMensaje] = useState([]);
   const [mensaje, setMensaje] = useState({
     Mensaje: "",
-    idCurso: auth.curso,
+    idCurso: auth.Descripción,
+    fecha: "",
   });
   const handleChange = (name, value) =>
     setMensaje({ ...mensaje, [name]: value });
+
   const handleSend = async () => {
     console.log(mensaje);
     console.log(auth);
@@ -41,7 +45,7 @@ const MensaggeScreen = ({ route }) => {
         Alert.alert("Registrado");
         setMensaje({
           Mensaje: "",
-          idCurso: auth.curso,
+          idCurso: auth.idCurso,
         });
         navegation.navigate("Aprendizaje");
         setLoading(false);
@@ -54,6 +58,7 @@ const MensaggeScreen = ({ route }) => {
       setLoading(false);
     }
   };
+
   const handle = async (id) => {
     try {
       await MensajeActualizar(id);
@@ -62,18 +67,27 @@ const MensaggeScreen = ({ route }) => {
     }
   };
   const renderItem = ({ item }) => {
+    const formattedDate = new Date(item.fecha).toLocaleDateString("es-ES");
     return (
-      <View
-        key={item.id}
+      <TouchableOpacity
+        onPress={() => {
+          handle(item.id);
+        }}
       >
-        <TouchableOpacity
-          onPress={() => {
-            handle(item.id);
-          }}
-        >
-          <Text style={style.cell}>{item.Mensaje}</Text>
-        </TouchableOpacity>
-      </View>
+        <View key={item.id} style={style.row}>
+          <View style={[style.cell, style.column]}>
+            <Text style={style.cellText}>{item.Mensaje}</Text>
+          </View>
+          {auth.IdRol !== 2 && (
+            <View style={[style.cell, style.column]}>
+              <Text style={style.cellText}>{auth.Descripción}</Text>
+            </View>
+          )}
+          <View style={[style.cell, style.column]}>
+            <Text style={style.cellText}>{formattedDate}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     );
   };
 
@@ -96,100 +110,147 @@ const MensaggeScreen = ({ route }) => {
       <View style={style.container}>
         <Name />
 
-        {auth.IdRol == 5 && (
-          <View style={style.campoTexto}>
-            <Text style={style.newMessage}>Mensaje Nuevo</Text>
-            <TextInput
-              style={style.texto}
-              placeholder="Nuevo  Mensaje..."
-              maxLength={60}
-              onChangeText={(text) => handleChange("Mensaje", text)}
-              value={mensaje.Mensaje}
-            />
-            <View style={style.iconos}>
-              <TouchableOpacity onPress={handleSend}>
-                <View style={style.send}>
-                  <ImageBackground
-                    source={enviar}
-                    style={style.icn}
-                  ></ImageBackground>
-                </View>
-              </TouchableOpacity>
-            </View>
-            <View>
-              {loading ? (
-                <ActivityIndicator size="large" color="#0000ff" />
-              ) : (
-                <Text></Text>
-              )}
+        {auth.IdRol === 5 ? (
+          <View style={style.containerMensaje}>
+            <View style={style.campoTexto}>
+              <Text style={style.newMessage}>Mensaje Nuevo</Text>
+              <TextInput
+                style={style.texto}
+                placeholder="Escribir..."
+                maxLength={60}
+                onChangeText={(text) => handleChange("Mensaje", text)}
+                value={mensaje.Mensaje}
+              />
+              <View style={style.iconos}>
+                <TouchableOpacity onPress={handleSend}>
+                  <View style={style.send}>
+                    <ImageBackground
+                      source={enviar}
+                      style={style.icn}
+                    ></ImageBackground>
+                  </View>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        )}
+        ) : null}
 
-        <FlatList
-          data={dataMensaje}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id.toString()}
-          style={style.table}
-        />
+        <View style={style.table}>
+          <View style={[style.row, style.headerRow]}>
+            <View style={[style.cell, style.column]}>
+              <Text style={style.headerCell}>TEMA</Text>
+            </View>
+            <View style={[style.cell, style.column]}>
+              <Text style={style.headerCell}>CURSO</Text>
+            </View>
+            <View style={[style.cell, style.column]}>
+              <Text style={style.headerCell}>FECHA</Text>
+            </View>
+          </View>
+          <FlatList
+            data={dataMensaje}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </View>
+
+        <View>
+          {loading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : (
+            <Text></Text>
+          )}
+        </View>
       </View>
     </Layout>
   );
 };
+
 const style = StyleSheet.create({
   container: {
-    width:'100%',
-    height:'100%'
+    width: "100%",
+    height: "100%",
   },
   newMessage: {
-    fontSize: 15,
+    fontSize: 11,
     fontWeight: "bold",
-    margin: 5,
+    margin: 10,
   },
   texto: {
     borderWidth: 2,
-    borderColor: "black",
-    width: "95%",
-    height:"50%",
+    borderColor: "#3cb371",
+    width: "100%",
     alignSelf: "center",
+    textAlign: "center",
+    padding: "10%",
+    backgroundColor:'white'
   },
   icn: {
     width: 30,
     height: 30,
-    margin: 15,
+    margin: 10,
+    alignSelf:'center'
   },
   send: {
-    alignSelf:"flex-end",
+    borderRadius: 70,
+    backgroundColor:'#e0ffff',
+    margin:5,
+    borderColor: "#3cb371",
+    borderWidth:2
   },
   table: {
     borderWidth: 2,
-    borderColor: "#ccc",
     marginVertical: 10,
-    width:"95%",
-    height:"100%",
-    alignSelf:'center',
-    paddingHorizontal: 10,
-    backgroundColor:'white',
-    paddingBottom:5
+    backgroundColor: "#f0ffff",
+    paddingBottom: 5,
+    borderLeftWidth: 2, // Agregar estilo para línea vertical izquierda
+    borderRightWidth: 2, // Agregar estilo para línea vertical derecha
+    borderColor: "white", // Color de las líneas verticales
+  },
+  headerRow: {
+    backgroundColor: "#3cb371",
+  },
+  headerCell: {
+    fontSize: 12,
+    fontWeight: "bold",
+    textAlign: "center",
+    color:'white'
   },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: 10,
     paddingVertical: 7,
   },
   cell: {
     flex: 1,
-    textAlign: "left",
-    fontSize: 12,
-    fontWeight: "bold",
-    padding:5,
-    borderBottomWidth: 1, // Agregar bordes inferiores a las celdas
-    borderBottomColor: '#ccc', // Cambiar el color de los bordes inferiores
+    padding: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    borderRightWidth: 1,
+    borderRightColor: "#ccc",
   },
-  iconos:{
-    alignSelf:"flex-end",
-  }
+  iconos: {
+    alignSelf: "flex-end",
+  },
+  column: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  cellText: {
+    fontSize: 11,
+    textAlign: "center",
+    fontWeight:'bold'
+  },
+  containerMensaje: {
+    height: "25%",
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
 });
 
 export default MensaggeScreen;
