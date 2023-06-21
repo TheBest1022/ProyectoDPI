96b070065eff8ceb7c55ed239e988b3349cd1aa9
TheBest1022 <fernando07101@outlook.com>
2023-06-20 23:42:41 -0500

    Actualizar

--- src/helpers/configuracion.js
+++ src/helpers/configuracion.js
@@ -2,4 +2,5 @@ const urlWeb = "https://app-backend-server.herokuapp.com/"
 const urlLocalCG = "http://192.168.0.108:4000/"
 const urlLocalTru = "http://192.168.0.3:4000/"
 const urilocal2 = "http://192.168.18.96:4000/"
-export const conexionURL = urlWeb
+const uriServer = "backend-production-7a1b.up.railway.app"
+export const conexionURL = uriServer
