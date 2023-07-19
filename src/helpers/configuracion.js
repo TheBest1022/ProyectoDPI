225fed77619f0e4576e75fba13a84e0455d9b278
TheBest1022 <fernando07101@outlook.com>
2023-07-19 12:47:45 -0500

    Actualizar

--- src/helpers/configuracion.js
+++ src/helpers/configuracion.js
@@ -3,4 +3,4 @@ const urlLocalCG = "http://192.168.0.108:4000/"
 const urlLocalTru = "http://192.168.0.3:4000/"
 const urilocal2 = "http://192.168.18.96:4000/"
 const uriServer = "backend-production-7a1b.up.railway.app"
-export const conexionURL = uriServer
+export const conexionURL = urlLocalTru
